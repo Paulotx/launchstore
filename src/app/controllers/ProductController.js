8acde3a580cb35888c8a75067b1a0a2abda49dbd
Paulotx <pauloandre0011@gmail.com>
2020-04-20 19:03:36 -0300

    Finalizando Gerenciador de imagens

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -2,6 +2,8 @@ const { formatPrice } = require("../../lib/utils");
 
 const Category = require("../models/Category");
 const Product  = require("../models/Product");
+const File     = require("../models/File");
+
 
 module.exports = {
     create(req, res) {
@@ -27,9 +29,11 @@ module.exports = {
             }
         }
 
-        if(req.body.category_id == 0) {
-            return res.send("Plese, select a chef!");
-        }
+        if(req.body.category_id == 0) 
+            return res.send("Plese, select a Category!");
+
+        if(req.files.length == "") 
+            return res.send("Please, send at least one image"); 
 
         const values = [
             req.body.category_id,
@@ -45,7 +49,10 @@ module.exports = {
         let results = await Product.create(values);
         const productId = results.rows[0].id;
 
-        return res.redirect(`/products/${ productId }`);
+        const filesPromisse = req.files.map(file => File.create([ file.filename, file.path, productId ]));
+        await Promise.all(filesPromisse);
+
+        return res.redirect(`/products/${ productId }/edit`);
     },
 
     async edit(req, res) {
@@ -56,19 +63,45 @@ module.exports = {
 
         product.price = formatPrice(product.price);
 
+        // get catefory
         results = await Category.all();
         const categories = results.rows;
 
-        return res.render("products/edit.njk", { product, categories });
+        // get images
+        results = await Product.files(product.id);
+        let files = results.rows;
+
+        files = files.map(file => ({ 
+            ...file,
+            src: `${ req.protocol }://${ req.headers.host }${ file.path.replace("public","") }`
+         }));
+
+        return res.render("products/edit.njk", { product, categories, files });
     },
 
     async put(req, res) {
         const keys = Object.keys(req.body);
 
         for(key of keys) {
-            if(req.body[key] == "") {
+            if(req.body[key] == "" && key != "removed_files") {
                 return res.send("Please, fill all fields!");
             }
+        }
+
+        if(req.files.length != 0) {
+            const newFilesPromisse = req.files.map(file => File.create([ file.filename, file.path, req.body.id ]));
+            await Promise.all(newFilesPromisse);
+        }
+
+        if(req.body.removed_files) {
+            const removedFiles = req.body.removed_files.split(",");
+            const lastIndex    = removedFiles.length - 1;
+
+            removedFiles.splice(lastIndex, 1);
+
+            const removedFilesPromise = removedFiles.map(id => File.delete(id));
+            
+            await Promise.all(removedFilesPromise);
         }
 
         if(req.body.old_price != req.body.price) {
