d6e63e653e2a5b7c789f1cf8af015228b0a73198
Paulotx <pauloandre0011@gmail.com>
2020-05-01 02:14:20 -0300

    Iniciando cadastro do Usuarios

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,9 +1,6 @@
 const { formatPrice } = require("../../lib/utils");
 
-const Category = require("../models/Category");
 const Product  = require("../models/Product");
-const File     = require("../models/File");
-
 
 module.exports = {
     async index(req, res) {
